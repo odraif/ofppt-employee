631eef9be355234344bfe55cb0d9e7832da31d9e
odraif <otmandraif@gmail.com>
2023-04-27 10:56:31 +0100

    update index.js

--- index.js
+++ index.js
@@ -2,27 +2,30 @@ const Express = require("express");
 const Employees = require("./db/employees");
 const app = Express();
 
-app.post("/create/employee",(req,res)=>{
-    const Employee = new Employees({
-        Nom: "mohamed",
-        Prenom:"hatim",
-        CIN:"js8523",
-        Email:"mohamedhatim@ofppt.ma",
-        Tel:"0645782315",
-        Date: "05/03/1993",
-        Type:"formateur",
-        Salaire:"5000dh",
-        Modules:[5,3,1]
-      });
-      
-      Employee.save()
-        .then(() => {
-          console.log('Employee saved successfully');
-        })
-        .catch(err => {
-          console.error('Error saving Employee', err);
+app.post("/create/employee", (req, res) => {
+    if (!req.body) {
+        return res.json({ message: "request error" });
+    } else {
+        const { Nom, Prenom, CIN, Email, Tel, Date, Type, Salaire, Modules } = req.body;
+        const newEmployee = new Employees({
+            Nom,
+            Prenom,
+            CIN,
+            Email,
+            Tel,
+            Date,
+            Type,
+            Salaire,
+            Modules
         });
+        newEmployee.save()
+            .then(() => {
+                console.log('Employee saved successfully');
+            })
+            .catch(err => {
+                console.error('Error saving Employee', err);
+            });
+    }
 });
-
 
 app.listen("3000");
